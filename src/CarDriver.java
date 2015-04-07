da60a8845e9b635f0976725bec6917cea5cb74cb
rdb24057 <rdb24057@an098060-ct103v.nvstu.edu>
2015-04-07 15:18:20 -0400

    CarDrive

---
+++ src/CarDriver.java
@@ -0,0 +1,17 @@
+
+public class CarDriver {
+
+	public static void main(String[] args) {
+		Car RaulCar = new Car();
+		System.out.println(RaulCar);
+		
+		Car Car1=new Car ("yellow", 1, 1, "Honda");
+		System.out.println(Car1);
+		
+		Car Car2=new Car ("White", 2, 2, "Toyota");
+		System.out.println(Car2);
+		
+
+	}
+
+}
